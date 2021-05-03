861d2d7f4a3d9ecb91aa6089ab9bb37c8b0a8779
nirajparsai <niraj.parsai@gmail.com>
2021-05-03 18:09:35 +0530

    Delete azCopy.sh

--- azCopy.sh
+++
@@ -1,8 +0,0 @@
-echo "azcopy login with SP"
-
-export AZCOPY_SPA_CLIENT_SECRET=$(client-secret)
-azcopy login --service-principal --application-id $(clientid) --tenant-id=$(tenentid)
-
-echo "azcopy Artifact to ADLS"
-
-azcopy copy "$(System.DefaultWorkingDirectory)/_nirajparsai_nwrlc/" "https://$(target-storage).dfs.core.windows.net/$(target-container)/DBPP/" --include-path "code/bin/py/Sparkpostprocess.py;code/bin/py/customPurge.py"  --recursive=true
