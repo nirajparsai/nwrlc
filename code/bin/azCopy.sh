67b066058846052edb23ebb7698beff36c005132
nirajparsai <niraj.parsai@gmail.com>
2021-05-17 21:21:22 +0530

    Update azCopy.sh

--- code/bin/azCopy.sh
+++ code/bin/azCopy.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 echo "azcopy Artifact to ADLS"
-azcopy copy "$System.DefaultWorkingDirectory/nwrlc/" "https://$target_storage.dfs.core.windows.net/$target_container/DBPP/" --include-path "code/bin/py/Sparkpostprocess.py;code/bin/py/customPurge.py"  --recursive=true
+azcopy copy "$System.DefaultWorkingDirectory/nwrlc/" "https://$target_storage.dfs.core.windows.net/$target_container/DBPP/" --include-path "code/bin/py/Sparkpostprocess.py;code/bin/py/customPurge.py" --recursive=true
