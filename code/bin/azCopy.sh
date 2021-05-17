b2536bd91c72e97a66024e640d9712086abe0646
nirajparsai <niraj.parsai@gmail.com>
2021-05-17 21:03:03 +0530

    Update azCopy.sh

--- code/bin/azCopy.sh
+++ code/bin/azCopy.sh
@@ -3,4 +3,4 @@ echo "azcopy login with SP :-" $clientidWK
 export AZCOPY_SPA_CLIENT_SECRET=$client-secret
 azcopy login --service-principal --application-id $clientid --tenant-id=$tenentid
 echo "azcopy Artifact to ADLS"
-azcopy copy "$(System.DefaultWorkingDirectory)/nwrlc/" "https://$target-storage.dfs.core.windows.net/$target-container/DBPP/" --include-path "code/bin/py/Sparkpostprocess.py;code/bin/py/customPurge.py"  --recursive=true
+azcopy copy "$(System.DefaultWorkingDirectory)/nwrlc/" "https://$target_storage.dfs.core.windows.net/$target_container/DBPP/" --include-path "code/bin/py/Sparkpostprocess.py;code/bin/py/customPurge.py"  --recursive=true
